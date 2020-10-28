cb1f7de11ecc8cce18a6af6c0540110ea539e3bd
Seán Coll <seancoll@Macbook-Air-of-Sean.local>
2020-10-28 22:45:56 +0000

    Fixed html for contact method

--- app_files/flaskApp.py
+++ app_files/flaskApp.py
@@ -14,25 +14,38 @@ try:
 except OSError:
     pass
 
+
 @app.route('/')
 def hello():
-    return """<!DOCTYPE html>
-        <html>
-        <head>
-	        <meta charset="utf-8"/>
-	        <title>Flask example Biography</title>
-        </head>
-        <body>
-	        <h1>About Me</h1>
-		        <div>Hello my name is Seán. I am a 3rd year student at Technological University Dublin.
-		        Visit (the current IP address)/contanct-me to contact me.</div>
-
-        </body>
-        </html>"""
+    return """
+    <!DOCTYPE html>
+    <html>
+    <head>
+	    <meta charset="utf-8"/>
+	    <title>Flask example Biography</title>
+    </head>
+    <body>
+	    <h1>About Me</h1>
+		    <div>Hello my name is Seán. I am a 3rd year student at Technological University Dublin.
+		    Visit (the current IP address)/contanct-me to contact me.</div>
+    </body>
+    </html>
+    """
 
 @app.route('contact-me')
 def contact():
-    return """<a href="mailto:c18332623@mytudublin.ie">Click to send me an email.</a>"""
+    return """
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <meta charset="utf-8"/>
+        <title>Contact me</title>
+    </head>
+    <body>
+        <a href="mailto:c18332623@mytudublin.ie">Click to send me an email.</a>
+    </body>
+    </html>
+    """
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
